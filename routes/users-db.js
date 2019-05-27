a89930ae880120bfa804ea3356a9525e178e3486
Varga Zsombor <zsombi1990@gmail.com>
2019-05-27 21:59:08 +0300

    Create, Delete, Read; next Update.

--- routes/users-db.js
+++ routes/users-db.js
@@ -16,6 +16,8 @@ router.get('/', function(req, res, next) {
     
     connection.query(sql, (err, results) => {
       res.json(results);
+
+      connection.release();
     });
   });
 });
@@ -27,28 +29,23 @@ router.post('/add', function(req, res, next) {
   var phoneNumber = req.body.phoneNumber;
   
   console.warn("Add: ", familyName, givenName, phoneNumber);
-  console.warn("Add: ", req.body.givenName);
-
-  // var strPeople = fs.readFileSync("./public/data/people.json");
-  // var people = JSON.parse(strPeople);
-  var people = require("../public/data/people.json"); // get the json type text file contents.
-
-  const id = new Date().getTime();
-  people.push({
-    id,
-    familyName,
-    givenName,
-    phoneNumber
-  });
-
-  var str = JSON.stringify(people, null, 2); // turn the acquired contents into string.
-  fs.writeFileSync("./public/data/people.json", str); // delete the file's old content and rewrite with the updated.
-
-  // TODO: save this data in the "people.json" file.
-  res.json({
-    success: true,
-    id,
-    message: "Done."
+  
+  pool.getConnection((err, connection) => {
+    const sql = `INSERT INTO contacts 
+      (id, familyName, givenName, phoneNumber)
+      VALUES (NULL, "${familyName}", "${givenName}", "${phoneNumber}")`;
+      
+    console.log(sql);
+    connection.query(sql, (err, result) => {
+      const id = result.insertId;
+      res.json({
+        success: true,
+        id,
+        message: "Done!"
+      });
+      
+      connection.release();
+    });
   });
 });
 
@@ -60,32 +57,24 @@ router.put('/update', function(req, res, next) {
   var phoneNumber = req.body.phoneNumber;
   
   console.warn("Update: ", id, familyName, givenName, phoneNumber);
-  console.warn("Body ID (Update): ", req.body.id);
-
-  // var strPeople = fs.readFileSync("./public/data/people.json");
-  // var people = JSON.parse(strPeople);
-  var people = require("../public/data/people.json"); // get the json type text file contents.
-
-  // Update
-  /* const person = people.find(function(p) {
-    return p.id == id;
-  }); */
-  const person = people.find((p) => {
-    return p.id == id;
-  });
-
-  person.familyName = familyName;
-  person.givenName = givenName;
-  person.phoneNumber = phoneNumber;
-
-  var str = JSON.stringify(people, null, 2); // turn the acquired contents into string.
-  fs.writeFileSync("./public/data/people.json", str); // delete the file's old content and rewrite with the updated.
-
-  // TODO: save this data in the "people.json" file.
-  res.json({
-    success: true,
-    id,
-    message: "Done."
+  
+  var id = req.body.id;
+    
+  console.warn("Remove: ", id);
+  
+  pool.getConnection((err, connection) => {
+    const sql = `UPDATE "contacts" 
+    SET "familyName" = "${familyName}", "givenName" = "${givenName}", "phoneNumber" = "${phoneNumber}"
+    WHERE "id" = "${id}"`;
+        
+    connection.query(sql, (err, results) => {
+      res.json({
+        success: true,
+        message: "Done!"
+      });
+        
+      connection.release();
+    });
   });
 });
 
@@ -95,20 +84,17 @@ router.delete('/delete', function(req, res, next) {
   
   console.warn("Remove: ", id);
 
-  // var strPeople = fs.readFileSync("./public/data/people.json");
-  // var people = JSON.parse(strPeople);
-  var people = require("../public/data/people.json"); // get the json type text file contents.
-
-  var remainingPeople = people.filter(function(person) {
-    return person.id != id;
-  });
-
-  var str = JSON.stringify(remainingPeople, null, 2); // turn the acquired contents into string.
-  fs.writeFileSync("./public/data/people.json", str); // delete the file's old content and rewrite with the updated.
-
-  res.json({
-    success: true,
-    message: "DONE."
+  pool.getConnection((err, connection) => {
+    const sql = `DELETE FROM contacts WHERE id = ${(id)}`;
+      
+    connection.query(sql, (err, results) => {
+      res.json({
+        success: true,
+        message: "Done!"
+      });
+      
+      connection.release();
+    });
   });
 });
 
